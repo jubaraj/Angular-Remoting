16e3c17d3ca7be288fb5253eb8684c3ba9deb72d
jubaraj <36961314+jubaraj@users.noreply.github.com>
2018-03-02 14:19:32 +0530

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.16
+:: Version: 1.0.15
 :: ----------------------
 
 :: Prerequisites
@@ -88,13 +88,12 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-
-
 :: 1. Select node version
 call :SelectNodeVersion
 
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  echo "Whatever"
   pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
@@ -103,19 +102,19 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 
 :: 3. Angular Prod Build //If you had generated this yourself then please add this step manually!!)
 IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
-  echo Building App in %DEPLOYMENT_SOURCE%...
-  pushd %DEPLOYMENT_SOURCE%
-  call:ExecuteCmd !NPM_CMD run Build
+echo Building App in %DEPLOYMENT_SOURCE%…
+pushd "%DEPLOYMENT_SOURCE%"
+call :ExecuteCmd !NPM_CMD! run build
 :: If the above command fails comment above and uncomment below one
-:: call ./node_modules/.bin/ng build --Prod
+:: call ./node_modules/.bin/ng build –prod
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
 
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
+    call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist/" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+    IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
